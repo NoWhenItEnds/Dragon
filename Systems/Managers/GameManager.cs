c1be81de21cf8c42250f7564e3d176ef7c6a520d
James Penn <JamesLockyerPenn@gmail.com>
2026-03-09 08:25:30 +0800

    feat: project initialisation.

---
+++ Systems/Managers/GameManager.cs
@@ -0,0 +1,15 @@
+using Godot;
+using Halcyon.Utilities.Singletons;
+
+namespace Dragon.Managers
+{
+    /// <summary> The game world's central manager singleton. </summary>
+    public partial class GameManager : SingletonNode<GameManager>
+    {
+        /// <inheritdoc/>
+        public override void _Ready()
+        {
+            GD.Print("Hello, World.");
+        }
+    }
+}
